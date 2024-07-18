6bccfd8131788a8d1950959dfe939ba7b1a06a8e
hurajgor <harsh.rajgor@copart.com>
2024-07-18 12:00:08 -0500

    feat: added pull to refresh functionality for rtl list
    Signed-off-by: hurajgor <harsh.rajgor@copart.com>

--- app/src/main/java/com/copart/rtlaisdk/ui/rtlList/composables/RTLListScreen.kt
+++ app/src/main/java/com/copart/rtlaisdk/ui/rtlList/composables/RTLListScreen.kt
@@ -1,18 +1,24 @@
 package com.copart.rtlaisdk.ui.rtlList.composables
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
+import androidx.compose.material3.pulltorefresh.PullToRefreshContainer
+import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -29,6 +35,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.onEach
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RTLListScreen(
     state: RTLListContract.State,
@@ -36,8 +43,16 @@ fun RTLListScreen(
     onEventSent: (event: RTLListContract.Event) -> Unit,
     onNavigationRequested: (navigationEffect: RTLListContract.Effect.Navigation) -> Unit
 ) {
+    val pullRefreshState = rememberPullToRefreshState()
     val snackBarHostState = remember { SnackbarHostState() }
     val snackBarMessage = stringResource(R.string.data_is_loaded)
+
+    if (pullRefreshState.isRefreshing) {
+        LaunchedEffect(SIDE_EFFECTS_KEY) {
+            onEventSent(RTLListContract.Event.Retry)
+            pullRefreshState.endRefresh()
+        }
+    }
 
     LaunchedEffect(SIDE_EFFECTS_KEY) {
         effectFlow?.onEach { effect ->
@@ -77,13 +92,20 @@ fun RTLListScreen(
         when {
             state.isLoading -> Progress()
             state.isError -> NetworkError { onEventSent(RTLListContract.Event.Retry) }
-            else -> RTLList(rtlList = state.rtlList) {
-                onEventSent(
-                    RTLListContract.Event.RTLListItemSelection(
-                        it
+            else ->
+                Box(modifier = Modifier.nestedScroll(pullRefreshState.nestedScrollConnection)) {
+                    RTLList(rtlList = state.rtlList) {
+                        onEventSent(
+                            RTLListContract.Event.RTLListItemSelection(
+                                it
+                            )
+                        )
+                    }
+                    PullToRefreshContainer(
+                        state = pullRefreshState,
+                        modifier = Modifier.align(Alignment.TopCenter)
                     )
-                )
-            }
+                }
         }
     }
 }
